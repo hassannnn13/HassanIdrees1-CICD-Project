f148a138affee8b24157fbae4ec91f6052a0cb93
M Hassan Idrees <hassanidrees093@gmail.com>
2026-06-24 22:19:36 +0500

    Test credentials fix

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ http.createServer((req, res) => {
   res.writeHead(200);
   res.end('Hello from Jenkins Pipeline! v2 - Hassan CICD Project');
 }).listen(3000, () => console.log('Running on port 3000'));
+// test
